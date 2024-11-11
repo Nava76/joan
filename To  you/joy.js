80e33a23f4a2f7d50821ef76192ba568cc4b123c
Nava76 <danielmureithi004@gmail.com>
2024-11-11 09:45:49 +0300

    Add files via upload

---
+++ To  you/joy.js
@@ -0,0 +1,7 @@
+document.getElementById('openLetterBtn').addEventListener('click', function() {
+    document.getElementById('letter').style.display = 'block';
+});
+
+document.getElementById('closeLetterBtn').addEventListener('click', function() {
+    document.getElementById('letter').style.display = 'none';
+});
